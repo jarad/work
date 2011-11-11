75d24407e7cd996c5cdaf452ec5589c1db927be8
Jarad Niemi <niemi@iastate.edu>
2011-11-11 13:35:10 -0600

    pass mu precision as argument

--- research/footballKickers/kickers.R
+++ research/footballKickers/kickers.R
@@ -15,17 +15,23 @@ kicker.by.name <- as.factor(rep(LETTERS[1:4], each=25))
 kicker <- as.numeric(kicker.by.name)
 y <- rbinom(N,1,1/(1+exp(-b[kicker,1]-b[kicker,2]*dist)))
 
-data <- list(N=length(y),K=length(unique(kicker)),y=y,
-             kicker=kicker,dist=dist,p=p,R=diag(p))
+data <- list(N=length(y),              # number of kicks
+             K=length(unique(kicker)), # number of kickers
+             y=y,                      # 1=kick good, 0=kick missed
+             kicker=kicker,            # kicker ID (numeric), must not have gaps
+             dist=dist,                # kick distance
+             p=p,                      # number of covariates including intercept
+             mu.prec=1e-6,             # overall mean precision
+             R=diag(p))                # Wishart location
 
 ### Create Initial Values
 
 inits <- list(list(b=b*0, mu=rep(0,2), Tau=diag(2)))
-parameters <- c("b","mu","Tau","Sigma")
+parameters <- c("b","mu","Tau")
 
 ## Run the simulation 
 
-nburn=1e4
+nburn=1e2
 kicker.sim <- bugs(data,inits,parameters,"kickermodel.txt",
                    n.chains=1,n.burnin=nburn,n.thin=1,n.iter=nburn*2)
 kicker.coda <- as.mcmc.list(kicker.sim)
