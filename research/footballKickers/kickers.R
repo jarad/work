6046d51fa4eba40bcf232291820a3a6a4c1c2410
Jarad Niemi <niemi@iastate.edu>
2011-11-11 13:27:07 -0600

    adjustable number of kicker specific parameters

--- research/footballKickers/kickers.R
+++ research/footballKickers/kickers.R
@@ -5,8 +5,9 @@ library(R2OpenBUGS)
 
 n.per.kicker <- 25
 K <- 4
-b <- matrix(rnorm(K*2),K,2)
-b[,2] <- 0
+p <- 2                       # Number of kicker specific parameters
+b <- matrix(rnorm(K*p),K,p)
+b[,p] <- 0
 N <- n.per.kicker*K
 
 dist <- runif(N,18,60)
@@ -14,19 +15,19 @@ kicker.by.name <- as.factor(rep(LETTERS[1:4], each=25))
 kicker <- as.numeric(kicker.by.name)
 y <- rbinom(N,1,1/(1+exp(-b[kicker,1]-b[kicker,2]*dist)))
 
-#data <- list(N=N,K=K,y=y,kicker=kicker,dist=dist, R=diag(2))
-data <- list(N=length(y),K=length(unique(kicker)),y=y,kicker=kicker,dist=dist,R=diag(2))
+data <- list(N=length(y),K=length(unique(kicker)),y=y,
+             kicker=kicker,dist=dist,p=p,R=diag(p))
 
 ### Create Initial Values
 
-#inits <- list(list(b=b*0, mu=rep(0,2), Tau=diag(2)))
 inits <- list(list(b=b*0, mu=rep(0,2), Tau=diag(2)))
 parameters <- c("b","mu","Tau","Sigma")
 
 ## Run the simulation 
 
 nburn=1e4
-kicker.sim <- bugs(data,inits,parameters,"kickermodel.txt",n.chains=1,n.burnin=nburn,n.thin=1,n.iter=nburn*2)
+kicker.sim <- bugs(data,inits,parameters,"kickermodel.txt",
+                   n.chains=1,n.burnin=nburn,n.thin=1,n.iter=nburn*2)
 kicker.coda <- as.mcmc.list(kicker.sim)
 #attach.all(kicker.sim$sims.list) 
 
